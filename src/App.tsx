5e76f940d10c1e8b923a0c364aebd149d4c7ad3e
Jose Espejo <joseespejo@yahoo.com>
2025-08-08 19:09:13 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+type Job = {
+  id: string;
+  title: string;
+  redirect_url: string;
+  company: { display_name: string };
+  location: { display_name: string };
+  description: string;
+};
+
+type Response = {
+  results: Job[];
+};
+
+function App() {
+  const [data, setData] = useState<Response | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=276f06d5&app_key=4d8ad3f833efde7607b09893735b52c7&results_per_page=500&what=frontend%20developer&where=london&content-type=application/json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const json: Response = await response.json();
+
+      setData({
+        results: json.results,
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Adzuna API Jobs</h1>
+        <ul>
+          {data.results.map((job) => (
+            <li key={job.id} className="mt-4">
+              <h2 className="font-bold">{job.title}</h2>
+              <p>{job.company.display_name}</p>
+              <p>{job.location.display_name}</p>
+              <p>{job.description}</p>
+              <a
+                href={job.redirect_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Job
+              </a>
+            </li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+
+  // return <pre>{JSON.stringify(data, null, 2)}</pre>;
+}
+
+export default App;
